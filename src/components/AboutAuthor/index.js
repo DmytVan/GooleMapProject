ad3ad1cfd73624abeb6065f906a303c730d23067
DmytVan <36083185+DmytVan@users.noreply.github.com>
2019-07-21 17:22:31 +0300

    Update index.js

--- src/components/AboutAuthor/index.js
+++ src/components/AboutAuthor/index.js
@@ -7,7 +7,7 @@ class AboutAuthor extends React.Component {
                 <p>Dima Ivanov. Man.</p>
                 <p>Contacts: </p>
                 <p>E-mail: <a href="mailto:dimon.iv997@gmail.com">dimon.iv997@gmail.com</a></p>
-                <p>Phone: <a href="tel:+380976610832"/>+380976610832</p>
+                <p>Phone: <a href="tel:+380976610832">+380976610832</a></p>
             </div>
         )
     }
